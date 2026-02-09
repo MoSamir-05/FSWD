410692aa5dc2aaa13a3c20e447376ced748909e3
MoSamir-05 <ps8949409@gmail.com>
2026-02-09 14:57:25 +0530

    09-02-26

---
+++ Node_js/vite-project/src/feedback.jsx
@@ -0,0 +1,4 @@
+function Feedback(props){
+    return
+
+}
---
+++ Node_js/vite-project/src/item.jsx
@@ -0,0 +1,14 @@
+function Item(props){
+    return<>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Cost</th>
+                <th>Type</th>
+            </tr>
+            <td>{props.name}</td>
+            <td>{props.cost}</td>
+            <td>{props.type}</td>
+        </table>
+    </>
+}
